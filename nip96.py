8775cb78b1e60d03dc04f8d25c2d5e62ec122d4e
lemonknowsall <lemonknowsall@gmail.com>
2024-08-31 16:38:44 -0700

    added loading spinner and bug fix to upload file

--- nip96.py
+++ nip96.py
@@ -7,34 +7,34 @@ def filenostrbuildupload(event_base64, filepath, caption, alt):
             "file": (filepath, file, "image/gif"),
         }
 
-    # Prepare headers with NIP-98 Authorization
-    headers = {
-        "Authorization": f"Nostr {event_base64}"
-    }
+        # Prepare headers with NIP-98 Authorization
+        headers = {
+            "Authorization": f"Nostr {event_base64}"
+        }
 
-    data = {
-        "caption": caption,
-        "expiration": "",  # "" for no expiration
-        "alt": alt,
-        "content_type": "image/gif",
-        "no_transform": "false"
-    }
+        data = {
+            "caption": caption,
+            "expiration": "",  # "" for no expiration
+            "alt": alt,
+            "content_type": "image/gif",
+            "no_transform": "false"
+        }
 
-    # Make the POST request without the Authorization header
-    api_url = "https://nostr.build/api/v2/nip96/upload"
-    response = requests.post(api_url, headers=headers, files=files, data=data)
+        # Make the POST request without the Authorization header
+        api_url = "https://nostr.build/api/v2/nip96/upload"
+        response = requests.post(api_url, headers=headers, files=files, data=data)
 
-    # Check the response
-    if response.status_code == 200:
-        print("File uploaded successfully.")
-        response = response.json()
-    else:
-        print("Failed to upload file.")
-        print("Status code:", response.status_code)
-        print("Response:", response.text)
-        response = response.text
+        # Check the response
+        if response.status_code == 200:
+            print("File uploaded successfully.")
+            response = response.json()
+        else:
+            print("Failed to upload file.")
+            print("Status code:", response.status_code)
+            print("Response:", response.text)
+            response = response.text
 
-    return response
+        return response
 
 def urlnostrbuildupload(event_base64, file_url, caption, alt):
     # Prepare headers with NIP-98 Authorization
